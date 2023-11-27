c6a97899fdc2c00c0a235ab91a705d2069855f20
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2023-11-27 22:07:00 +0600

    next step

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi magnam
+        accusantium quas quibusdam voluptatibus saepe delectus obcaecati
+        temporibus voluptatum quidem expedita repellat, distinctio nesciunt ea
+        suscipit nobis, rem voluptas asperiores!
+      </p>
+    </div>
+  );
+}
+
+export default Page;
